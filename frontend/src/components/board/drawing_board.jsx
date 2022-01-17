8cf2ddaf67c7ed1ca029839ee8383656922aa344
ashes4trees <stephsoni18@gmail.com>
2022-01-17 14:39:14 -0500

    start drawing board component

---
+++ frontend/src/components/board/drawing_board.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import p5 from 'p5';
+
+class DrawingBoard extends React.Component {
+   constructor(props) {
+       super(props)
+   }
+
+   createBoard(p) {
+       p.setup
+   }
+}
+
+export default DrawingBoard;
