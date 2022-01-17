a5b828668eb261e87630d9b3366aa614bff3cb10
Andrew Vitovitch <alvitovitch@gmail.com>
2022-01-17 14:05:48 -0500

    room splash page

--- frontend/src/components/room/roomContainer.js
+++ frontend/src/components/room/roomContainer.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux';
 import Room from './room';
 const mSTP = state => {
     return{
+        // players: Object.values(state.entities.users)
     }
 }
 
 const mDTP = dispatch => {
     return{
-
+        // create board
+        // update board
     }
 }
 
