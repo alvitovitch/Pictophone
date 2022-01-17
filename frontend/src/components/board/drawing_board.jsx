a1d9510aba92907a98a67f285cdf0437567554b3
ashes4trees <stephsoni18@gmail.com>
2022-01-17 18:04:52 -0500

    add sketch function

--- frontend/src/components/board/drawing_board.jsx
+++ frontend/src/components/board/drawing_board.jsx
@@ -7,50 +7,51 @@ import sketch from './sketch';
 class DrawingBoard extends React.Component{
     constructor(props) {
         super(props)
+        this.sketch = this.sketch.bind(this);
     }
 
-    setup(p) {
-        let canvas;
-        p.setup = () => canvas = p.createCanvas(500, 500)
-    }
-
-    draw(p) {
-        p.draw = () => {
-            p.background('red');
-            if (p.mouseIsPressed) {
-                p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-                p.strokeWeight(10);
-                p.fill('white');
-
-            }
-        }
-    }
-
-    // sketch(p5) {
+    // setup(p) {
     //     let canvas;
-    //     p5.setup = () => {
-    //         canvas = p5.createCanvas(500, 500);
-    //         // canvas.parent(document.querySelector('.canvas-container'));
-           
-    //     }
-    //     p5.draw = () => {
-    //         p5.background('red');
-    //         if (p5.mouseIsPressed) {
-    //             p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-    //             p5.strokeWeight(10);
-    //             p5.fill('white');
-               
+    //     p.setup = () => canvas = p.createCanvas(500, 500)
+    // }
+
+    // draw(p) {
+    //     p.draw = () => {
+    //         p.background('red');
+    //         if (p.mouseIsPressed) {
+    //             p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+    //             p.strokeWeight(10);
+    //             p.fill('white');
+
     //         }
     //     }
     // }
+
+    sketch(p5) {
+        let canvas;
+        p5.setup = () => {
+            canvas = p5.createCanvas(500, 500);
+            // canvas.parent(document.querySelector('.canvas-container'));
+           
+        }
+        p5.draw = () => {
+            p5.background('red');
+            if (p5.mouseIsPressed) {
+                p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+                p5.strokeWeight(10);
+                p5.fill('white');
+               
+            }
+        }
+    }
 
     render() {
         const board = this.setup(p5);
         board.draw(p5);
         return (
             <div className='canvas-container'>
-                <P5>sketch={board}</P5>
-                {/* <ReactP5Wrapper sketch={board}></ReactP5Wrapper> */}
+                
+                <ReactP5Wrapper sketch={this.sketch}></ReactP5Wrapper>
             </div>
         )
     }
