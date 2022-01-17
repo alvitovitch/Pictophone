6f9abd5b45da13ad944d2b33a9653c32ac943c1c
kyle <keginzburg@gmail.com>
2022-01-16 23:35:53 -0500

    Write user login and register routes along with passport auth token functionality

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema);
