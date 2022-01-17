f2d9eb2400cb60d4e9b3350d981006763fd2e035
Andrew Vitovitch <alvitovitch@gmail.com>
2022-01-17 00:02:11 -0500

    styling navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -2,25 +2,45 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import logo from "../../images/vintage-candlestick-telephone-vector-21717680.jpg"
 
-
 class NavBar extends React.Component {
 
     render() {
-        debugger
-        return (
-            <div id='navBar'>
-                <div>
-                    <div id='Logo'>
-                        <img src={logo} />    
+        // if (this.props.loggedIn) {
+            return (
+                <div id='navBar'>
+                    <div id='logoTitle'>
+                        <div id='Logo'>
+                            <img id='logoPic' src={logo} />    
+                        </div>
+                    Pictophone
                     </div>
-                Pictophone
+                    <div>
+                        <button id='loginButton'>
+                            <Link to={'/login'}>Login</Link>
+                        </button>
+                        <button id='signupButton'>
+                            <Link to={'/signup'}>Sign up</Link>
+                        </button>
+                    </div>
+
                 </div>
-                <div>
-                    <Link to={'/login'}>Login</Link>
-                    <Link to={'/signup'}>Sign up</Link>
-                </div>
-            </div>
-        )
+            )
+        // } else {
+        //     return(
+        //         <div id='navBar'>
+        //             <div id='logoTitle'>
+        //                 <div id='Logo'>
+        //                     <img id='logoPic' src={logo} />    
+        //                 </div>
+        //             Pictophone
+        //             </div>
+        //             <div>
+        //                 <Link to={'/login'}>Login</Link>
+        //                 <Link to={'/signup'}>Sign up</Link>
+        //             </div>
+                
+        //     )
+        // }
     }
 } 
 
