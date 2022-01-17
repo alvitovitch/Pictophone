0191bacd524420895036c0f3aea6f9cd8eb5aaf5
Andrew Vitovitch <alvitovitch@gmail.com>
2022-01-16 23:09:46 -0500

    working on splash

--- frontend/src/components/navbar/navbar_container.js
+++ frontend/src/components/navbar/navbar_container.js
@@ -1,3 +1,17 @@
 import { connect } from 'react-redux';
 import NavBar from './navbar';
 
+const mSTP = state => {
+    return{
+
+    }
+}
+
+const mDTP = dispatch => {
+    return{
+
+    }
+}
+
+
+export default connect(mSTP, mDTP)(NavBar)
---
+++ frontend/src/components/splash/splash.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Splash extends React.Component {
+
+    remder() {
+        return (
+            <div>
+                this is the splash
+            </div>
+        )
+    }
+}
+
+export default Splash
