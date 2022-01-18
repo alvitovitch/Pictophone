86d498bb28428a54b41de18ee63c2077e670e0ec
Andrew Vitovitch <alvitovitch@gmail.com>
2022-01-18 10:56:49 -0500

    updated keys

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Alvitovitch:DPjsaceqwDjL7Kk@cluster0.ybtcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: 'xntDjBE5Lt'
+              //Make sure this is your own unique string
+}
