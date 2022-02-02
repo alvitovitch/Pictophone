b22788819fe508a336df6431be3dbda1744234c8
kyle <keginzburg@gmail.com>
2022-02-02 16:03:00 -0500

    Footer styling

--- frontend/src/components/linkbar/linkBar.jsx
+++ frontend/src/components/linkbar/linkBar.jsx
@@ -7,54 +7,85 @@ class LinkBar extends React.Component {
         return (
             <div id="linkBar">
                 <div className="personalLinks">
-                    <div className="linkName">
-                        Alex Dziuba
+                    <div className="tintype-container">
+                        <img src="images/alex_d.jpg" alt="" />
+                        <h6>A.D.</h6>
                     </div>
-                    <div>
+                    <div className="links">
+                        {/* <div className="linkName">
+                            Alex Dziuba
+                        </div> */}
                         <a href='https://github.com/AlexD89'>
-                            <img className="githubImg" src='images/GitHub-Mark.png' alt="image"/>
+                            <img className="githubImg" src='images/GitHub-Mark.svg' alt="github icon"/>
                         </a>
-                        <a href='www.linkedin.com/in/alexander-dziuba-0426a0122'>
-                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.png"/>
+                        <a href="https://www.linkedin.com/in/alex-dziuba-0426a0122/">
+                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.svg" alt="linkedin icon"/>
+                        </a>
+                        <a href="https://angel.co/u/alex-dziuba">
+                            <img className="angellistImg" src="images/angellist_icon.svg" alt="angellist" />
                         </a>
                     </div>
                 </div>
                 <div className="personalLinks">
-                    <div className="linkName">
-                        Andrew Vitovitch
+                    <div className="tintype-container">
+                        <img src="images/andrew_v.jpg" alt="" />
+                        <h6>A.V.</h6>
                     </div>
-                    <div>
+                    <div className="links">
+                        {/* <div className="linkName">
+                            Andrew Vitovitch
+                        </div> */}
                         <a href='https://github.com/alvitovitch'>
-                            <img className="githubImg" src="images/GitHub-Mark.png" alt="image"/>
+                            <img className="githubImg" src='images/GitHub-Mark.svg' alt="github icon" />
                         </a>
                         <a href='https://www.linkedin.com/in/alvitovitch/'>
-                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.png" alt="image"/>
+                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.svg" alt="linkedin icon" />
+                        </a>
+                        <a href="https://angel.co/u/andrew-vitovitch">
+                            <img className="angellistImg" src="images/angellist_icon.svg" alt="angellist" />
                         </a>
                     </div>
                 </div>
                 <div className="personalLinks">
-                    <div className="linkName">
-                        Kyle Ginzburg
+                    <div className="tintype-container">
+                        <img src="images/kyle_g.JPG" alt="" />
+                        <h6>K.G.</h6>
                     </div>
-                    <div>
+                    <div className="links">
+                        {/* <div className="linkName">
+                            Kyle Ginzburg
+                        </div> */}
                         <a href='https://github.com/keginzburg'>
-                            <img className="githubImg" src="images/GitHub-Mark.png" alt="image"/>
+                            <img className="githubImg" src='images/GitHub-Mark.svg' alt="github icon" />
+                            
                         </a>
                         <a href='https://www.linkedin.com/in/kyleginzburg/'>
-                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.png" alt="image"/>
+                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.svg" alt="linkedin icon" />
+                            
+                        </a>
+                        <a href="https://angel.co/u/kyle-ginzburg">
+                            <img className="angellistImg" src="images/angellist_icon.svg" alt="angellist" />
+                            
                         </a>
                     </div>
                 </div>
                 <div className="personalLinks">
-                    <div className="linkName">
-                        Stephanie Soni
+                    <div className="tintype-container">
+                        <img src="images/steph_s.jpg" alt="" />
+                        <h6>S.S.</h6>
                     </div>
-                    <div>
+                    <div className="links">
+                        {/* <div className="linkName">
+                            Stephanie Soni
+                        </div> */}
                         <a href='https://github.com/ashes4trees'>
-                            <img className="githubImg" src="images/GitHub-Mark.png"/>
+                            <img className="githubImg" src='images/GitHub-Mark.svg' alt="github icon" />
                         </a>
                         <a href='https://www.linkedin.com/in/steph-soni/'>
-                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.png"/>
+                            <img className="linkedinImg" src="images/LinkedIn_logo_initials.svg" alt="linkedin icon" />
+                        </a>
+                        <a href="https://angel.co/u/steph-soni">
+                            <img className="angellistImg" src="images/angellist_icon.svg" alt="angellist" />
                         </a>
                     </div>
                 </div>
