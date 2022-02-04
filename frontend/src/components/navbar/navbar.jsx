046ea6ca54f6dfa6fce55aaa09a75d92e6861b3c
Andrew Vitovitch <alvitovitch@gmail.com>
2022-02-04 14:03:30 -0500

    added sliders for sound effects and styled them

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -8,7 +8,6 @@ class NavBar extends React.Component {
         this.smoothJazz = new Audio('audio/smoothJazz.mp3')
         this.smoothJazz.loop = true
         this.smoothJazz.volume = .2
-        this.smoothJazz.muted = 'muted'
     }
 
     componentDidMount() {
@@ -16,6 +15,17 @@ class NavBar extends React.Component {
         this.volume.addEventListener("change", ()=> {
             this.smoothJazz.volume = this.volume.value / 100;
         })
+    }
+
+    handleClick(e) {
+        if (e.target === document.getElementById('settings-modal'))
+        {
+            e.target.style.display = 'none'
+        }
+    }
+
+    showSettings(){
+        document.getElementById('settings-modal').style.display = 'flex'
     }
 
     handleLogout = (e) => {
@@ -42,20 +52,23 @@ class NavBar extends React.Component {
             this.smoothJazz.currentTime = 0
             return (
                 <div id='navBar'>
+                    <div id='settings-modal' onClick={e => this.handleClick(e)}>
+                        <div id='settings-content'>
+                            <div className="settings-option">
+                                <h3>Sound Effects</h3>
+                                <input type="range" id="sound-control"/>
+                            </div>
+                            <div className="settings-option">
+                                <h3>Music</h3>
+                                <input type="range" id="music-control"/>
+                            </div>
+                        </div>
+                    </div>
                     <div id='logoTitle'>
                         <div id='Logo'>
                             {/* <img id='logoPic' src="/images/transparent_phone.png" />   */}
                             <img id='logoPic' src="/images/vintage_phone.png" /> 
-                            <div>
-                                <div>
-                                    <h3>Sound Effects</h3>
-                                    <input type="range" id="sound-control"/>
-                                </div>
-                                <div>
-                                    <h3>Music</h3>
-                                    <input type="range" id="music-control"/>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                     <Link to={'/'} className="main-title">Pictophone</Link>
@@ -74,26 +87,28 @@ class NavBar extends React.Component {
             this.smoothJazz.play() 
             return(
                 <div id='navBar'>
+                    <div id='settings-modal' onClick={e => this.handleClick(e)}>
+                        <div id='settings-content'>
+                            <div className="settings-option">
+                                <h3>Sound Effects</h3>
+                                <input type="range" id="sound-control"/>
+                            </div>
+                            <div className="settings-option">
+                                <h3>Music</h3>
+                                <input type="range" id="music-control"/>
+                            </div>
+                        </div>
+                    </div>
                     <div id='logoTitle'>
                         <div id='Logo'>
                             {/* <img id='logoPic' src="/images/transparent_phone.png" /> */}
                             <img id='logoPic' src="/images/vintage_phone.png" />
-                            <div>
-                                <div>
-                                    <h3>Sound Effects</h3>
-                                    <input type="range" id="sound-control"/>
-                                </div>
-                                <div>
-                                    <h3>Music</h3>
-                                    <input type="range" id="music-control"/>
-                                </div>
-                            </div>
                         </div>
                     </div>
                         <Link to={'/'} className="main-title">Pictophone</Link>
                     <div id='navButtons'>
                         {/* <button className="navButton">Profile</button> */}
-                        {/* <button className="navButton">Settings</button> */}
+                        <button onClick={this.showSettings} className="navButton">Settings</button>
                         <button className="navButton" onClick={this.handleLogout}>logout</button>
                     </div>
                 </div>
